1b10fbb1f308be01dd0ac6286e9e5b25e092e5e0
Dukantic <aubindo@outlook.fr>
2025-01-01 21:17:02 +0100

    better blocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,15 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
-
 static const Block blocks[] = {
-    /* Icon       Command                                       Update Interval    Update Signal */
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-    {"", "date '+%Y-%m-%d %H:%M:%S'",                           60,                1},
-    {"", "acpi | awk '{print $4}'",                             30,                2},
-    {"", "amixer get Master | grep -o '[0-9]*%'",               10,                3},
+
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
-
-
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
