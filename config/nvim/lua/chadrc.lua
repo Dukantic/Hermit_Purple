b18c3a2e4bb867799ca4c16fe1508b99e228b23b
Dukantic <aubindo@outlook.fr>
2025-02-21 23:49:33 +0100

    change config for color in nvim

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -6,11 +6,11 @@
 local M = {}
 
 M.base46 = {
-	theme = "radium",
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	theme = "ayu_dark",
+	hl_override = {
+	Comment = { italic = true },
+	["@comment"] = { italic = true },
+	},
 }
 
 -- M.nvdash = { load_on_startup = true }
