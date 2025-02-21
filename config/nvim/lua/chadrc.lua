355b8afe2a7ec5e8149594c457acd8d2a962b998
Dukantic <aubindo@outlook.fr>
2025-02-21 22:58:41 +0100

    change neovim

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -6,12 +6,18 @@
 local M = {}
 
 M.base46 = {
-	theme = "mito-laser",
-
+	theme = "radium",
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
 }
+
+-- M.nvdash = { load_on_startup = true }
+-- M.ui = {
+--       tabufline = {
+--          lazyload = false
+--      }
+--}
 
 return M
